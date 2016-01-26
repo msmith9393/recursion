588ec9c9118fdaf831d2d8daf5005d71978cb68c
Megan Smith <megan@tokaypress.com>
2016-01-26 10:57:30 -0800

    Complete getElementsByClassName using inner function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -13,8 +13,20 @@ var getElementsByClassName = function(className){
 
   // create an inner function that will search through all the nodes and determine if they have a class named className
   function traverse(node) {
-
+  	// check if node has className
+  	if (node.classList && node.classList.contains(className)) {
+  		// if it does add it to hasClass
+  		hasClass.push(node);
+  	}
+  	// loop through all the nodes children
+  	for (var i=0; i<node.children.length; i++) {
+  		// recursively call traverse on each child node
+  		traverse(node.children[i])
+  	}
   }
+  
+  // invoke traverse on the document body so it will begin searching from the top
+  traverse(node);
 
   // return the array storing all the nodes that have a class named className
   return hasClass;
