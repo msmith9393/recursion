f1f790fffcd1da23aac34c37893cafe7a1d9fd00
Megan Smith <megan@tokaypress.com>
2016-01-26 11:16:58 -0800

    stringifyJSON works on numbers, booleans, strings, and null

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,15 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	// if type of obj is a number, a boolean, or obj is null
+	if (typeof obj === "number" || typeof obj === "boolean" ||  obj === null) {
+		// return stringified obj
+		return obj + '';
+	}
+	// if type of obj is a string
+  if (typeof obj === "string") {
+  	// return string beginning and ending with additional quotation marks
+  	return '"' + obj + '"';
+  }
 };
