db39b2ca2639c6c7e42627adaeb378c2e7470429
Megan Smith <megan@tokaypress.com>
2016-01-26 10:49:00 -0800

    Layout of getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  
+  // start our search at the node document.body so that we search through all the nodes
+  var node = document.body;
+  // an array to store all the nodes that have a class named className
+  var hasClass = [];
+
+  // create an inner function that will search through all the nodes and determine if they have a class named className
+  function traverse(node) {
+
+  }
+
+  // return the array storing all the nodes that have a class named className
+  return hasClass;
+
 };
